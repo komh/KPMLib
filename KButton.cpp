e0a25027a922068b0bc5cd5db229627ad8caaef2
KO Myung-Hun <komh@chollian.net>
2012-08-29 16:26:57 +0900

    Implement CreateWindow() for KButton class

--- KButton.cpp
+++ KButton.cpp
@@ -8,3 +8,14 @@ KButton::~KButton()
 {
 }
 
+bool KButton::CreateWindow( const KWindow* pkwndP, PCSZ pcszName,
+                            ULONG flStyle, LONG x, LONG y, LONG cx, LONG cy,
+                            const KWindow* pkwndO, const KWindow* pkwndS,
+                            ULONG id, PVOID pCtlData, PVOID pPresParams )
+{
+    SetClassName( PMLITERAL( WC_BUTTON ));
+
+    return KWindow::CreateWindow( pkwndP, pcszName, flStyle, x, y, cx, cy,
+                                  pkwndO, pkwndS, id, pCtlData, pPresParams );
+}
+
--- KButton.h
+++ KButton.h
@@ -8,5 +8,12 @@ class KButton : public KWindow
 public :
     KButton();
     virtual ~KButton();
+
+
+    virtual bool CreateWindow( const KWindow* pkwndP, PCSZ pcszName,
+                               ULONG flStyle, LONG x, LONG y,
+                               LONG cx, LONG cy, const KWindow* pkwndO,
+                               const KWindow* pkwndS, ULONG id,
+                               PVOID pCtlData = 0, PVOID pPresParams = 0 );
 };
 #endif
